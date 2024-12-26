3d053dad500415f5c0ebabb2f488f7ef24389483
Rojina Sapkota <159830132+RojinaSk@users.noreply.github.com>
2024-12-26 13:48:08 -0600

    notes update

--- Iris_species_prediction/Model_I_code.r
+++ Iris_species_prediction/Model_I_code.r
@@ -1,6 +1,6 @@
 # Importing libraries
 library(RCurl) # for downloading the iris CSV file
-library(randomForest) #to run the model
+library(randomForest) #to create the prediction model
 library(caret) #for data splitting
 
 #we will be using the iris dataset already in Rstudio
@@ -8,15 +8,15 @@ library(caret) #for data splitting
 # Performs stratified random split of the data set
 TrainingIndex <- createDataPartition(iris$Species, p=0.8, list = FALSE)
 TrainingSet <- iris[TrainingIndex,] # Training Set
-TestingSet <- iris[-TrainingIndex,] # Test Set
+TestingSet <- iris[-TrainingIndex,] #remainder 0.2 as Testing Set
 
-write.csv(TrainingSet, "training.csv")
+write.csv(TrainingSet, "training.csv") #saving for preventing shuffling and allowing reproducibility
 write.csv(TestingSet, "testing.csv")
 
 TrainSet <- read.csv("training.csv", header = TRUE)
 TrainSet <- TrainSet[,-1]
 
-# Building Random forest model
+# Building Random forest model and saving into model
 
 model <- randomForest(Species ~ ., data = TrainSet, ntree = 500, mtry = 4, importance = TRUE)
 
